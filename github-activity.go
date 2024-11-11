d33b0ba6d2a2618a6151fcfa3299d3c66d1125bf
andrey_dergunov <andrei_dergunov@epam.com>
2024-11-11 20:36:35 +0100

    basic working version

--- github-activity.go
+++ github-activity.go
@@ -14,8 +14,8 @@ func main() {
 		fmt.Println(os.Args[0], "<username>")
 		return
 	}
-
-	url := fmt.Sprintf("https://api.github.com/users/%s/events", os.Args[1])
+	username := os.Args[1]
+	url := fmt.Sprintf("https://api.github.com/users/%s/events", username)
 	body, _, err := http_handler.Get(url)
 	if err != nil {
 		log.Fatalf("Failed to fetch %v\n", err)
@@ -26,6 +26,24 @@ func main() {
 		log.Fatalf("Failed to decode json %v\n", err)
 	}
 
-	fmt.Println(activities)
+	fmt.Printf("%s activity:\n", username)
+	for _, activity := range activities.Objects {
+		switch activity.Type {
+		case "PushEvent":
+			fmt.Printf("- %s Pushed %d commits to %s\n", activity.CreatedAt, len(activity.Payload.Commits), activity.Repo.Name)
+		case "CreateEvent":
+			fmt.Printf("- %s Created %s %s at %s\n", activity.CreatedAt, activity.Payload.RefType, activity.Payload.Ref, activity.Repo.Name)
+		case "PullRequestEvent":
+			fmt.Printf("- %s Updated PR status to %s (%s)\n", activity.CreatedAt, activity.Repo.Name, activity.Payload.Action)
+		case "PullRequestReviewCommentEvent":
+			fmt.Printf("- %s Updated PR review comment status to %s (%s)\n", activity.CreatedAt, activity.Repo.Name, activity.Payload.Action)
+		case "PullRequestReviewEvent":
+			fmt.Printf("- %s Updated PR review status to %s (%s)\n", activity.CreatedAt, activity.Repo.Name, activity.Payload.Action)
+		case "IssuesEvent":
+			fmt.Printf("- %s Updated issue status at %s (%s)\n", activity.CreatedAt, activity.Repo.Name, activity.Payload.Issue.State)
+		case "DeleteEvent":
+			fmt.Printf("- %s Deleted %s %s at %s\n", activity.CreatedAt, activity.Payload.RefType, activity.Payload.Ref, activity.Repo.Name)
+		}
+	}
 
 }
--- json_handler/json_handler.go
+++ json_handler/json_handler.go
@@ -21,11 +21,16 @@ type Activity struct {
 				Email string `json:"email"`
 			} `json:"author"`
 		} `json:"commits,omitempty"`
+		Ref     string `json:"ref,omitempty"`
+		RefType string `json:"ref_type,omitempty"` // fo CreateEvent
+		Issue   struct {
+			State string `json:"state,omitempty"`
+		} `json:"issue,omitempty"` // IssuesEvent
 	} `json:"payload"`
 }
 
 type Activities struct {
-	Activities []Activity
+	Objects []Activity
 }
 
 // stringer for individual activities
@@ -40,18 +45,23 @@ func (a Activity) String() string {
 // stringer for a slice of activities
 func (as Activities) String() string {
 	var result string = "[\n"
-	for i, a := range as.Activities {
+	for i, a := range as.Objects {
 		obj_s := a.String()
 		result += obj_s
-		if len(obj_s) > 0 && i < len(as.Activities)-1 {
+		if len(obj_s) > 0 && i < len(as.Objects)-1 {
 			result += ",\n"
 		}
 	}
 	return result + "\n]"
 }
 
-func ParseActivities(data []byte) (Activities, error) {
+func ParseActivitiesAsArray(data []byte) ([]Activity, error) {
 	activities := make([]Activity, 0)
 	err := json.Unmarshal(data, &activities)
+	return activities, err
+}
+
+func ParseActivities(data []byte) (Activities, error) {
+	activities, err := ParseActivitiesAsArray(data)
 	return Activities{activities}, err
 }
